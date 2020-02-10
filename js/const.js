bc83356db6fc64d07e173693d9ca2dda577e6ba1
xiuming <xiuming@mit.edu>
2020-02-10 18:40:03 -0500

    reusable constants

---
+++ js/const.js
@@ -0,0 +1,132 @@
+function getConst(id) {
+    switch(id) {
+        case "me":
+            document.write("<u>Xiuming Zhang</u>");
+            break;
+        case "bill_hikey":
+            document.write("<a target='_blank' href=\"http://billf.mit.edu/\">William T. Freeman</a>");
+            break;
+        case "bill":
+            document.write("<a class=\"lowkey\" target='_blank' href=\"http://billf.mit.edu/\">William T. Freeman</a>");
+            break;
+        case "mit":
+            document.write("<a target='_blank' href=\"http://www.mit.edu\">Massachusetts Institute of Technology (MIT)</a>");
+            break;
+        case "google_res":
+            document.write("<a target='_blank' href=\"https://research.google/\">Google Research</a>");
+            break;
+        case "yunta_hikey":
+            document.write("<a target='_blank' href=\"https://ai.google/research/people/105312\">Yun-Ta Tsai</a>");
+            break;
+        case "jon_hikey":
+            document.write("<a target='_blank' href=\"https://jonbarron.info/\">Jonathan T. Barron</a>");
+            break;
+        case "tianfan_hikey":
+            document.write("<a target='_blank' href=\"https://people.csail.mit.edu/tfxue/\">Tianfan Xue</a>");
+            break;
+        case "thomas_hikey":
+            document.write("<a target='_blank' href=\"https://sites.google.com/view/yeolab/thomas\">B. T. Thomas Yeo</a>");
+            break;
+        case "mosculp-bbc":
+            document.write("<a target='_blank' href=\"https://www.bbc.com/news/av/technology-45552015/creating-3d-sculptures-from-2d-video-and-other-news\">BBC</a>");
+            break;
+        case "mosculp-forbes":
+            document.write("<a target='_blank' href=\"https://www.forbes.com/sites/jenniferhicks/2018/09/25/these-researchers-turned-2d-videos-into-3d-motion-sculptures\">Forbes</a>");
+            break;
+        case "mosculp-mit":
+            document.write("<a target='_blank' href=\"https://news.mit.edu/2018/creating-3-d-printed-motion-sculptures-from-2-d-videos-mit-csail-0919\">MIT</a>");
+            break;
+        case "mosculp-homepage":
+            document.write("<a target='_blank' href=\"http://mosculp.csail.mit.edu/assets/homepage.png\">the 9/19 MIT homepage</a>");
+            break;
+        case "ad-eurekalert":
+            document.write("<a target='_blank' href=\"http://www.eurekalert.org/pub_releases/2016-10/mgh-dba100716.php\">EurekAlert!</a>");
+            break;
+        case "ad-mgh":
+            document.write("<a target='_blank' href=\"http://www.massgeneral.org/about/pressrelease.aspx?id=2002\">MGH/HMS</a>");
+            break;
+        case "ad-nus":
+            document.write("<a target='_blank' href=\"http://news.nus.edu.sg/highlights/model-illuminates-alzheimers-disease\">NUS</a>");
+            break;
+        case "jiayuan":
+            document.write("<a class=\"lowkey\" target='_blank' href=\"http://jiayuanm.com/\">Jiayuan Mao</a>");
+            break;
+        case "yikai":
+            document.write("<a class=\"lowkey\" target='_blank' href=\"https://scholar.google.com/citations?user=0EVzwD8AAAAJ&hl=en\">Yikai Li</a>");
+            break;
+        case "josh":
+            document.write("<a class=\"lowkey\" target='_blank' href=\"http://web.mit.edu/cocosci/josh.html\">Joshua B. Tenenbaum</a>");
+            break;
+        case "jiajun":
+            document.write("<a class=\"lowkey\" target='_blank' href=\"https://jiajunwu.com/\">Jiajun Wu</a>");
+            break;
+        case "iccv":
+            document.write("<a class=\"lowkey\" target='_blank' href=\"https://en.wikipedia.org/wiki/International_Conference_on_Computer_Vision\">ICCV</a>")
+            break;
+        case "siyi":
+            document.write("<a class=\"lowkey\" target='_blank' href=\"https://scholar.google.com/citations?user=dpRSfnoAAAAJ&hl=en\">Siyi Tang</a>");
+            break;
+        case "nanbo":
+            document.write("<a class=\"lowkey\" target='_blank' href=\"https://scholar.google.com.sg/citations?user=odaBvWAAAAAJ&hl=en\">Nanbo Sun</a>");
+            break;
+        case "thomas":
+            document.write("<a class=\"lowkey\" target='_blank' href=\"https://sites.google.com/view/yeolab/thomas\">B. T. Thomas Yeo</a>");
+            break;
+        case "beth":
+            document.write("<a class=\"lowkey\" target='_blank' href=\"https://med.stanford.edu/profiles/elizabeth-mormino\">Elizabeth C. Mormino</a>");
+            break;
+        case "zhoutong":
+            document.write("<a class=\"lowkey\" target='_blank' href=\"https://ztzhang.info/\">Zhoutong Zhang</a>");
+            break;
+        case "chengkai":
+            document.write("<a class=\"lowkey\" target='_blank' href=\"https://scholar.google.com/citations?user=rChGGwgAAAAJ&hl=en\">Chengkai Zhang</a>");
+            break;
+        case "nips":
+            document.write("<a class=\"lowkey\" target='_blank' href=\"https://en.wikipedia.org/wiki/Conference_on_Neural_Information_Processing_Systems\">NeurIPS</a>");
+            break;
+        case "tali":
+            document.write("<a class=\"lowkey\" target='_blank' href=\"http://people.csail.mit.edu/talidekel/\">Tali Dekel</a>");
+            break;
+        case "tianfan":
+            document.write("<a class=\"lowkey\" target='_blank' href=\"https://people.csail.mit.edu/tfxue/\">Tianfan Xue</a>");
+            break;
+        case "andrew":
+            document.write("<a class=\"lowkey\" target='_blank' href=\"http://andrewowens.com/\">Andrew Owens</a>");
+            break;
+        case "qiurui":
+            document.write("<a class=\"lowkey\" target='_blank' href=\"https://scholar.google.com/citations?user=BxqV_RsAAAAJ&hl=en\">Qiurui He</a>");
+            break;
+        case "stefanie":
+            document.write("<a class=\"lowkey\" target='_blank' href=\"http://stefaniemueller.org/\">Stefanie Mueller</a>");
+            break;
+        case "uist":
+            document.write("<a class=\"lowkey\" target='_blank' href=\"https://en.wikipedia.org/wiki/ACM_Symposium_on_User_Interface_Software_and_Technology\">UIST</a>");
+            break;
+        case "eccv":
+            document.write("<a class=\"lowkey\" target='_blank' href=\"https://en.wikipedia.org/wiki/European_Conference_on_Computer_Vision\">ECCV</a>");
+            break;
+        case "xingyuan":
+            document.write("<a class=\"lowkey\" target='_blank' href=\"http://people.csail.mit.edu/xingyuan/\">Xingyuan Sun</a>");
+            break;
+        case "cvpr":
+            document.write("<a class=\"lowkey\" target='_blank' href=\"https://en.wikipedia.org/wiki/Conference_on_Computer_Vision_and_Pattern_Recognition\">CVPR</a>");
+            break;
+        case "reisa":
+            document.write("<a class=\"lowkey\" target='_blank' href=\"https://www.nmr.mgh.harvard.edu/user/5647\">Reisa A. Sperling</a>");
+            break;
+        case "mert":
+            document.write("<a class=\"lowkey\" target='_blank' href=\"http://sabuncu.engineering.cornell.edu/\">Mert R. Sabuncu</a>");
+            break;
+        case "pnas":
+            document.write("<a class=\"lowkey\" target='_blank' href=\"https://en.wikipedia.org/wiki/Proceedings_of_the_National_Academy_of_Sciences_of_the_United_States_of_America\">PNAS</a>");
+            break;
+        case "ismrm":
+            document.write("<a target='_blank' href=\"https://www.ismrm.org/2016-annual-meeting-exhibition/\">ISMRM 2016</a>");
+            break;
+        case "ad-upi":
+            document.write("<a target='_blank' href=\"http://www.upi.com/Health_News/2016/10/07/Brain-atrophy-patterns-may-explain-diversity-in-Alzheimers-symptoms/9771475867874/\">UPI</a>");
+            break;
+        default:
+            document.write("UNDEFINED");
+    }
+}
